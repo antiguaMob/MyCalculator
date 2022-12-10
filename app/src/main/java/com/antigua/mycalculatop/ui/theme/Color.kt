f5597c2e0084500fe9b72567bf128c401c978d16
antiguaMob <antigua_m@mail.ru>
2022-12-10 16:07:29 +0300

    Lesson 1

--- app/src/main/java/com/antigua/mycalculatop/ui/theme/Color.kt
+++ app/src/main/java/com/antigua/mycalculatop/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val DeepBlue = Color(0xFF243C58)
+val DeepBlue500 = Color(0xFF111D2B)
+val DeepBlue200 = Color(0xFF4571A5)
+val Gold = Color(0xFFFFA41F)
